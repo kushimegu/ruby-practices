6ae2a029be2bae3630b29a89568991cb28494b69
Kushizaki <kushimegu288@gmail.com>
2024-06-20 15:45:41 +0900

    mainメソッドを短くリファクタリング

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -3,67 +3,48 @@
 
 require 'optparse'
 
-def count_lines(data)
-  format('%4d', data.count("\n"))
+def main
+  options = ARGV.getopts('lwc')
+  if ARGV.empty?
+    input_data = $stdin.read
+    input_lines, input_words, input_bytes = calculate_stats(input_data)
+    output_with_options(input_lines, input_words, input_bytes, options)
+  else
+    files = ARGV
+    total_stats = { lines: 0, words: 0, bytes: 0 }
+    files.each_with_object(total_stats) do |file, totals|
+      file_data = File.read(file)
+      file_lines, file_words, file_bytes = calculate_stats(file_data)
+      output_with_options(file_lines, file_words, file_bytes, options)
+      puts
+      totals[:lines] += file_lines
+      totals[:words] += file_words
+      totals[:bytes] += file_bytes
+    end
+    formatted_stats = total_stats.transform_values { |stat| four_digit_format(stat) }
+    print "#{formatted_stats[:lines]} #{formatted_stats[:words]} #{formatted_stats[:bytes]} total" if files.size > 1
+  end
 end
 
-def count_words(data)
-  format('%4d', data.split.size)
-end
-
-def calculate_bytes(data)
-  format('%4d', data.size)
-end
-
-def list_options(options)
-  { l: options['l'], w: options['w'], c: options['c'] }
-end
-
-def sort_output(lines, words, bytes)
-  { l: lines, w: words, c: bytes }
+def calculate_stats(data)
+  lines = data.count("\n")
+  words = data.split.size
+  bytes = data.size
+  [lines, words, bytes]
 end
 
 def output_with_options(lines, words, bytes, options)
-  list_options(options).each do |option, is_true|
-    print "#{sort_output(lines, words, bytes)[option]} " if is_true
-  end
+  formatted_lines = four_digit_format(lines)
+  formatted_words = four_digit_format(words)
+  formatted_bytes = four_digit_format(bytes)
+  print "#{formatted_lines} " if options['l']
+  print "#{formatted_words} " if options['w']
+  print "#{formatted_bytes} " if options['c']
+  print "#{formatted_lines} #{formatted_words} #{formatted_bytes}" if options.values.none?
 end
 
-options = ARGV.getopts('lwc')
-if ARGV == []
-  input_data = $stdin.read
-  input_lines = count_lines(input_data)
-  input_words = count_words(input_data)
-  input_bytes = calculate_bytes(input_data)
-  if list_options(options).values.uniq.length == 1
-    print "#{input_lines} #{input_words} #{input_bytes}"
-  else
-    output_with_options(input_lines, input_words, input_bytes, options)
-  end
-else
-  files = ARGV
-  files.each do |file|
-    file_data = File.read(file)
-    file_lines = count_lines(file_data)
-    file_words = count_words(file_data)
-    file_bytes = calculate_bytes(file_data)
-    if list_options(options).values.uniq.length == 1
-      print "#{file_lines} #{file_words} #{file_bytes} #{file}"
-    else
-      output_with_options(file_lines, file_words, file_bytes, options)
-      print file
-    end
-    puts
-  end
-  if files.size > 1
-    total_lines = format('%4d', files.sum { |file| count_lines(File.read(file)).to_i })
-    total_words = format('%4d', files.sum { |file| count_words(File.read(file)).to_i })
-    total_bytes = format('%4d', files.sum { |file| calculate_bytes(File.read(file)).to_i })
-    if list_options(options).values.uniq.length == 1
-      print "#{total_lines} #{total_words} #{total_bytes} total"
-    else
-      output_with_options(total_lines, total_words, total_bytes, options)
-      print 'total'
-    end
-  end
+def four_digit_format(number)
+  format('%4d', number)
 end
+
+main
