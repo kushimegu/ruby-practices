95bb2f25b33649ebd67e587238bae7a3602f3f40
Kushizaki <kushimegu288@gmail.com>
2024-06-21 14:21:32 +0900

    ファイル名と合計表示の修正

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -6,23 +6,23 @@ require 'optparse'
 def main
   options = ARGV.getopts('lwc')
   if ARGV.empty?
-    input_data = $stdin.read
-    input_lines, input_words, input_bytes = calculate_stats(input_data)
+    input_lines, input_words, input_bytes = calculate_stats($stdin.read)
     output_with_options(input_lines, input_words, input_bytes, options)
   else
     files = ARGV
     total_stats = { lines: 0, words: 0, bytes: 0 }
     files.each_with_object(total_stats) do |file, totals|
-      file_data = File.read(file)
-      file_lines, file_words, file_bytes = calculate_stats(file_data)
+      file_lines, file_words, file_bytes = calculate_stats(File.read(file))
       output_with_options(file_lines, file_words, file_bytes, options)
-      puts
+      puts file
       totals[:lines] += file_lines
       totals[:words] += file_words
       totals[:bytes] += file_bytes
     end
-    formatted_stats = total_stats.transform_values { |stat| four_digit_format(stat) }
-    print "#{formatted_stats[:lines]} #{formatted_stats[:words]} #{formatted_stats[:bytes]} total" if files.size > 1
+    if files.size > 1
+      output_with_options(total_stats[:lines], total_stats[:words], total_stats[:bytes], options)
+      print 'total'
+    end
   end
 end
 
@@ -40,7 +40,7 @@ def output_with_options(lines, words, bytes, options)
   print "#{formatted_lines} " if options['l']
   print "#{formatted_words} " if options['w']
   print "#{formatted_bytes} " if options['c']
-  print "#{formatted_lines} #{formatted_words} #{formatted_bytes}" if options.values.none?
+  print "#{formatted_lines} #{formatted_words} #{formatted_bytes} " if options.values.none?
 end
 
 def four_digit_format(number)
