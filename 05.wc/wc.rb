fed13becd05b3e2c6104118f31e5c7ce74597dad
Kushizaki <kushimegu288@gmail.com>
2024-06-24 11:25:29 +0900

    ステータス算出メソッドのハッシュ化

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -6,44 +6,43 @@ require 'optparse'
 def main
   options = ARGV.getopts('lwc')
   if ARGV.empty?
-    input_lines, input_words, input_bytes = calculate_stats($stdin.read)
-    output_with_options(input_lines, input_words, input_bytes, options)
+    input_stats = calculate_stats($stdin.read)
+    output_with_options(input_stats, options)
   else
     files = ARGV
     total_stats = { lines: 0, words: 0, bytes: 0 }
     files.each_with_object(total_stats) do |file, totals|
-      file_lines, file_words, file_bytes = calculate_stats(File.read(file))
-      output_with_options(file_lines, file_words, file_bytes, options)
+      file_stats = calculate_stats(File.read(file))
+      output_with_options(file_stats, options)
       puts file
-      totals[:lines] += file_lines
-      totals[:words] += file_words
-      totals[:bytes] += file_bytes
+      totals[:lines] += file_stats[:lines]
+      totals[:words] += file_stats[:words]
+      totals[:bytes] += file_stats[:bytes]
     end
     if files.size > 1
-      output_with_options(total_stats[:lines], total_stats[:words], total_stats[:bytes], options)
-      print 'total'
+      output_with_options(total_stats, options)
+      puts 'total'
     end
   end
 end
 
-def calculate_stats(data)
-  lines = data.count("\n")
-  words = data.split.size
-  bytes = data.size
-  [lines, words, bytes]
+def calculate_stats(input_data)
+  count_lines = input_data.count("\n")
+  count_words = input_data.split.size
+  calculate_bytes = input_data.size
+  { lines: count_lines, words: count_words, bytes: calculate_bytes }
 end
 
-def output_with_options(lines, words, bytes, options)
-  formatted_lines = four_digit_format(lines)
-  formatted_words = four_digit_format(words)
-  formatted_bytes = four_digit_format(bytes)
-  print "#{formatted_lines} " if options['l']
-  print "#{formatted_words} " if options['w']
-  print "#{formatted_bytes} " if options['c']
-  print "#{formatted_lines} #{formatted_words} #{formatted_bytes} " if options.values.none?
+def output_with_options(stats, options)
+  formatted_lines = digit_format(stats[:lines])
+  formatted_words = digit_format(stats[:words])
+  formatted_bytes = digit_format(stats[:bytes])
+  print "#{formatted_lines} " if options['l'] || options.values.none?
+  print "#{formatted_words} " if options['w'] || options.values.none?
+  print "#{formatted_bytes} " if options['c'] || options.values.none?
 end
 
-def four_digit_format(number)
+def digit_format(number)
   format('%4d', number)
 end
 
