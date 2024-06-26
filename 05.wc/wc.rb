5eff28695cf512e5bd90d77b08306a29b945764c
Kushizaki <kushimegu288@gmail.com>
2024-06-26 15:56:31 +0900

    メソッドのリファクタリング

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -10,26 +10,28 @@ def main
     output_with_options(stats, options)
   else
     files = ARGV
-    total_stats = { lines: 0, words: 0, bytes: 0 }
+    total_stats = { lines: 0, words: 0, bytes: 0, name: 'total' }
     files.each do |file|
-      stats = calculate_stats(File.read(file))
-      output_with_options(stats, options, file)
+      stats = calculate_stats(File.read(file), file)
+      output_with_options(stats, options)
       total_stats[:lines] += stats[:lines]
       total_stats[:words] += stats[:words]
       total_stats[:bytes] += stats[:bytes]
     end
-    output_with_options(total_stats, options, 'total') if files.size > 1
+    output_with_options(total_stats, options) if files.size > 1
   end
 end
 
-def calculate_stats(text)
-  count_lines = text.count("\n")
-  count_words = text.split.size
-  calculate_bytes = text.size
-  { lines: count_lines, words: count_words, bytes: calculate_bytes }
+def calculate_stats(text, file = nil)
+  {
+    lines: text.count("\n"),
+    words: text.split.size,
+    bytes: text.size,
+    name: file
+  }
 end
 
-def output_with_options(stats, options, file_or_total = nil)
+def output_with_options(stats, options)
   no_option = options.values.none?
   formatted_lines = format_digits(stats[:lines])
   print "#{formatted_lines} " if options['l'] || no_option
@@ -37,7 +39,7 @@ def output_with_options(stats, options, file_or_total = nil)
   print "#{formatted_words} " if options['w'] || no_option
   formatted_bytes = format_digits(stats[:bytes])
   print "#{formatted_bytes} " if options['c'] || no_option
-  puts file_or_total if file_or_total
+  puts stats[:name]
 end
 
 def format_digits(number)
