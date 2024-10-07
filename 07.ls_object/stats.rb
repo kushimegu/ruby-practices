a3882ec0366ac484b8b9e181c1a6f3172ab91937
Kushizaki <kushimegu288@gmail.com>
2024-10-07 14:28:07 +0900

    時間フォーマットのメソッドを引数なしに変更

--- 07.ls_object/stats.rb
+++ 07.ls_object/stats.rb
@@ -6,6 +6,7 @@ require 'date'
 class Stats
   def initialize(file)
     @file = file
+    @stats = File.stat(@file)
   end
 
   FILE_TYPE_MAP = {
@@ -30,26 +31,25 @@ class Stats
   }.freeze
 
   def list
-    stats = File.stat(@file)
-    mode = format('%06o', stats.mode)
+    mode = format('%06o', @stats.mode)
     type = FILE_TYPE_MAP[mode[0, 2]]
     mode_owner = FILE_MODE_MAP[mode[3]]
     mode_group = FILE_MODE_MAP[mode[4]]
     mode_other = FILE_MODE_MAP[mode[5]]
-    link = format('%2d', stats.nlink)
-    user_name = Etc.getpwuid(stats.uid).name
-    group_name = Etc.getgrgid(stats.gid).name
-    size = format('%4d', stats.size)
-    mtime = stats.mtime
-    month = format('%2d', mtime.month)
-    day = format('%2d', mtime.day)
-    time_or_year = format_time_or_year(mtime)
+    link = format('%2d', @stats.nlink)
+    user_name = Etc.getpwuid(@stats.uid).name
+    group_name = Etc.getgrgid(@stats.gid).name
+    size = format('%4d', @stats.size)
+    month = format('%2d', @stats.mtime.month)
+    day = format('%2d', @stats.mtime.day)
+    time_or_year = format_time_or_year
     [type + mode_owner + mode_group + mode_other, link, user_name, group_name, size, month, day, time_or_year, @file]
   end
 
   private
 
-  def format_time_or_year(mtime)
+  def format_time_or_year
+    mtime = @stats.mtime
     now = Time.now
     today = now.to_date
     if mtime.to_date >= today.prev_month(6) && mtime < now
